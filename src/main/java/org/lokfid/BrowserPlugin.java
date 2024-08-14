917d2a52b6490b464b89a8ac4b2fde26477e0c74
Lokfid <n.pushkareva_82@mail.ru>
2024-08-14 14:29:09 +0500

    Initial commit

---
+++ src/main/java/org/lokfid/BrowserPlugin.java
@@ -0,0 +1,30 @@
+package org.lokfid;
+
+import org.rusherhack.client.api.RusherHackAPI;
+import org.rusherhack.client.api.plugin.Plugin;
+
+/**
+ * Example rusherhack plugin
+ *
+ * @author John200410
+ */
+public class BrowserPlugin extends Plugin {
+	
+	@Override
+	public void onLoad() {
+		
+		//logger
+		this.getLogger().info("Web plugin loaded!");
+		
+		//creating and registering a new module
+		final BrowserModule exampleModule = new BrowserModule();
+		RusherHackAPI.getModuleManager().registerFeature(exampleModule);
+
+	}
+	
+	@Override
+	public void onUnload() {
+		this.getLogger().info("Web plugin unloaded!");
+	}
+	
+}
